e59005433988fd6d0b47cdcbf55fb6a16cc03c73
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2022-03-05 17:56:12 +0530

    map ui improved

--- frontend/billion-stars-frontend/pages/Map.js
+++ frontend/billion-stars-frontend/pages/Map.js
@@ -52,102 +52,121 @@ export default function Map() {
   };
 
   return (
-    <div className="relative bg-gradient-to-r from-[#0f172a] to-[#112a64] overflow-hidden">
-      <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-10 lg:pb-28">
-        <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:static">
-          <Listbox value={selected} onChange={setSelected}>
-            {({ open }) => (
-              <>
-                <Listbox.Label className="block text-sm font-medium text-white">
-                  Select City
-                </Listbox.Label>
-                <div className="mt-1 relative">
-                  <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                    <span className="flex items-center">
-                      <span className="ml-3 block truncate">
-                        {selected.name}
-                      </span>
-                    </span>
-                    <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                      <SelectorIcon
-                        className="h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                      />
-                    </span>
-                  </Listbox.Button>
+    <>
+      <div className="relative bg-gradient-to-r from-[#0f172a] to-[#112a64] overflow-hidden">
+        <div>
+          <div>
+            <Listbox value={selected} onChange={setSelected}>
+              {({ open }) => (
+                <>
+                  <div className="grid grid-cols-1 gap-0  sm:grid-cols-4">
+                    <div className="col-span-4 md:col-span-1 bg-[#0f172a] px-4 py-5">
+                      {" "}
+                      <Listbox.Label className="block text-sm font-medium text-white">
+                        Select City
+                      </Listbox.Label>
+                      <div className="mt-1 relative">
+                        <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                          <span className="flex items-center">
+                            <span className="ml-3 block truncate">
+                              {selected.name}
+                            </span>
+                          </span>
+                          <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                            <SelectorIcon
+                              className="h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                          </span>
+                        </Listbox.Button>
 
-                  <Transition
-                    show={open}
-                    as={Fragment}
-                    leave="transition ease-in duration-100"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                      {cities.map((person) => (
-                        <Listbox.Option
-                          key={person.id}
-                          className={({ active }) =>
-                            classNames(
-                              active
-                                ? "text-white bg-indigo-600"
-                                : "text-gray-900",
-                              "cursor-default select-none relative py-2 pl-3 pr-9"
-                            )
-                          }
-                          value={person}
+                        <Transition
+                          show={open}
+                          as={Fragment}
+                          leave="transition ease-in duration-100"
+                          leaveFrom="opacity-100"
+                          leaveTo="opacity-0"
                         >
-                          {({ selected, active }) => (
-                            <>
-                              <div className="flex items-center">
-                                <span
-                                  className={classNames(
-                                    selected ? "font-semibold" : "font-normal",
-                                    "ml-3 block truncate"
-                                  )}
-                                >
-                                  {person.name}
-                                </span>
-                              </div>
+                          <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                            {cities.map((person) => (
+                              <Listbox.Option
+                                key={person.id}
+                                className={({ active }) =>
+                                  classNames(
+                                    active
+                                      ? "text-white bg-indigo-600"
+                                      : "text-gray-900",
+                                    "cursor-default select-none relative py-2 pl-3 pr-9"
+                                  )
+                                }
+                                value={person}
+                              >
+                                {({ selected, active }) => (
+                                  <>
+                                    <div className="flex items-center">
+                                      <span
+                                        className={classNames(
+                                          selected
+                                            ? "font-semibold"
+                                            : "font-normal",
+                                          "ml-3 block truncate"
+                                        )}
+                                      >
+                                        {person.name}
+                                      </span>
+                                    </div>
 
-                              {selected ? (
-                                <span
-                                  className={classNames(
-                                    active ? "text-white" : "text-indigo-600",
-                                    "absolute inset-y-0 right-0 flex items-center pr-4"
-                                  )}
-                                >
-                                  <CheckIcon
-                                    className="h-5 w-5"
-                                    aria-hidden="true"
-                                  />
-                                </span>
-                              ) : null}
-                            </>
-                          )}
-                        </Listbox.Option>
-                      ))}
-                    </Listbox.Options>
-                  </Transition>
-                </div>
-              </>
-            )}
-          </Listbox>
-          <div className="pt-10 pb-10">
-            <button
-              onClick={handleClick}
-              className="inline-block text-center bg-indigo-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-indigo-700"
-            >
-              Track pollution
-            </button>
-          </div>
-          <div className="pt-20 pb-10">
-            {cityInfo && (
-              <CityMap latitude={cityInfo.lat} longitude={cityInfo.lon} />
-            )}
+                                    {selected ? (
+                                      <span
+                                        className={classNames(
+                                          active
+                                            ? "text-white"
+                                            : "text-indigo-600",
+                                          "absolute inset-y-0 right-0 flex items-center pr-4"
+                                        )}
+                                      >
+                                        <CheckIcon
+                                          className="h-5 w-5"
+                                          aria-hidden="true"
+                                        />
+                                      </span>
+                                    ) : null}
+                                  </>
+                                )}
+                              </Listbox.Option>
+                            ))}
+                          </Listbox.Options>
+                        </Transition>
+                      </div>
+                      <div className="pt-10 pb-10">
+                        <button
+                          onClick={handleClick}
+                          className="inline-block text-center bg-indigo-600 border border-transparent rounded-md py-2 px-5 font-light text-white hover:bg-indigo-700 float-right"
+                        >
+                          fetch sensors
+                        </button>
+                      </div>
+                      <p className="text-center text-white pt-10">
+                        "Sensor information will be shown here"
+                      </p>
+                    </div>
+                    <div className="col-span-4 md:col-span-3">
+                      <div className="pt-0 pb-0" style={{ height: "100vh" }}>
+                        {cityInfo && (
+                          <CityMap
+                            latitude={cityInfo.lat}
+                            longitude={cityInfo.lon}
+                          />
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </>
+              )}
+            </Listbox>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
