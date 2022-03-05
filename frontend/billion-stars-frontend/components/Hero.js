86c3f66192d837ba0353a99f27e37852f8f6c2ca
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2022-03-05 10:53:22 +0530

    basic landing page added

---
+++ frontend/billion-stars-frontend/components/Hero.js
@@ -0,0 +1,97 @@
+import Link from "next/link";
+
+/* This example requires Tailwind CSS v2.0+ */
+export default function Hero() {
+  return (
+    <div className="relative bg-gradient-to-r from-[#0f172a] to-[#112a64] overflow-hidden">
+      <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
+        <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:static">
+          <div className="sm:max-w-lg">
+            <h1 className="text-4xl font font-extrabold tracking-tight text-white sm:text-6xl">
+              We are on a Mission
+            </h1>
+            <p className="mt-4 text-xl text-gray-500">
+              to make all our rivers live again, bring back those who have
+              become rare only because of us
+            </p>
+          </div>
+          <div>
+            <div className="mt-10">
+              {/* Decorative image grid */}
+              <div
+                aria-hidden="true"
+                className="pointer-events-none lg:absolute lg:inset-y-0 lg:max-w-7xl lg:mx-auto lg:w-full"
+              >
+                <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                  <div className="flex items-center space-x-6 lg:space-x-8">
+                    <div className="flex-shrink-0 grid grid-cols-1 gap-y-6 lg:gap-y-8">
+                      <div className="w-44 h-64 rounded-lg overflow-hidden sm:opacity-0 lg:opacity-100">
+                        <img
+                          src="https://images.unsplash.com/photo-1552561618-4fe029f7858a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=494&q=80"
+                          alt=""
+                          className="w-full h-full object-center object-cover"
+                        />
+                      </div>
+                      <div className="w-44 h-64 rounded-lg overflow-hidden">
+                        <img
+                          src="https://images.unsplash.com/photo-1609330861277-c66c42cf0916?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                          alt=""
+                          className="w-full h-full object-center object-cover"
+                        />
+                      </div>
+                    </div>
+                    <div className="flex-shrink-0 grid grid-cols-1 gap-y-6 lg:gap-y-8">
+                      <div className="w-44 h-64 rounded-lg overflow-hidden">
+                        <img
+                          src="https://images.unsplash.com/photo-1609109238958-eb5130c99873?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                          alt=""
+                          className="w-full h-full object-center object-cover"
+                        />
+                      </div>
+                      <div className="w-44 h-64 rounded-lg overflow-hidden">
+                        <img
+                          src="https://images.unsplash.com/photo-1623053058154-52b984d1d826?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80"
+                          alt=""
+                          className="w-full h-full object-center object-cover"
+                        />
+                      </div>
+                      <div className="w-44 h-64 rounded-lg overflow-hidden">
+                        <img
+                          src="https://images.unsplash.com/photo-1552012106-529e49caf1e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Z2FuZ2F8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+                          alt=""
+                          className="w-full h-full object-center object-cover"
+                        />
+                      </div>
+                    </div>
+                    <div className="flex-shrink-0 grid grid-cols-1 gap-y-6 lg:gap-y-8">
+                      <div className="w-44 h-64 rounded-lg overflow-hidden">
+                        <img
+                          src="https://images.unsplash.com/photo-1571989237340-98fb838eeef1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGdhbmdhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+                          alt=""
+                          className="w-full h-full object-center object-cover"
+                        />
+                      </div>
+                      <div className="w-44 h-64 rounded-lg overflow-hidden">
+                        <img
+                          src="https://images.unsplash.com/photo-1622810820879-976812797db9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                          alt=""
+                          className="w-full h-full object-center object-cover"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <a
+                href="#"
+                className="inline-block text-center bg-indigo-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-indigo-700"
+              >
+                Track pollution
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/billion-stars-frontend/components/Layout.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Navbar from "./Navbar";
+function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>Dolphin</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <Navbar />
+      {children}
+    </>
+  );
+}
+
+export default Layout;
